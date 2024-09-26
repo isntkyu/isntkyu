903a9f55f905354762d99713a273ec8029a63d91
이준규 <jkjkjk5710@gmail.com>
2024-09-26 18:14:27 +0900

    not wait load

--- getSession.js
+++ getSession.js
@@ -43,10 +43,10 @@ export const getVelogCookie = async (loginType) => {
   await page.type("#password", githubPw);
   await page.click("input[type='submit']");
 
-  await page.waitForNavigation({
-    timeout: 100000,
-    waitUntil: 'load'
-  });
+  // await page.waitForNavigation({
+  //   timeout: 100000,
+  //   waitUntil: 'load'
+  // });
 
   // await page.waitForSelector('body > div > div.HomeLayout_block__ZqnqH > div.responsive_mainResponsive___uG64 > div > header > div > div.Header_right__IaiY4 > a.Header_notification__cTNS6',{
   //   timeout: 100000,
@@ -55,7 +55,7 @@ export const getVelogCookie = async (loginType) => {
   // const url2 = await page.$eval('body > div > div.HomeLayout_block__ZqnqH > div.responsive_mainResponsive___uG64 > div > header > div > div.Header_right__IaiY4 > a.Header_notification__cTNS6', el => el.href);
   // await page.goto(url2);
 
-  await new Promise(resolve => setTimeout(resolve, 30000))
+  await new Promise(resolve => setTimeout(resolve, 50000))
   const cookies = await page.cookies()
 
   const cookieString = cookies.map(cookie => {
